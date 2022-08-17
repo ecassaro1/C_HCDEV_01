a81c889be276a142dd4190155c8f0feb9c212339
ecassaro1 <eric.cassaro@gmail.com>
2022-08-17 18:23:28 +0000

    add uma procedure

--- tiny1/README.md
+++ tiny1/README.md
@@ -1,1 +1,3 @@
 minha versão do TinyWorld
+
+o arquivo launch.json tem uma config pra DEBUG
--- tiny1/srv/index.js
+++ tiny1/srv/index.js
@@ -8,6 +8,10 @@ app.get('/Ent1', async function (req, res) {
     let rows = await getEnt1();
     res.send(rows);
 })
+app.get('/get_ent1_proc', async function (req, res) {
+    let rows = await getEnt1Proc();
+    res.send(rows);
+})
 
 const getEnt1 = async ()=>{
     //return [ { ID: 1, NAME: 'Hum Fake' }, { ID: 2, NAME: 'Dois Fake' } ];
@@ -32,7 +36,32 @@ const getEnt1 = async ()=>{
     return rows;
 }
 
+const getEnt1Proc = async ()=>{
+    //return [ { ID: 1, NAME: 'Hum Fake' }, { ID: 2, NAME: 'Dois Fake' } ];
+    var hana = require('@sap/hana-client');
+
+    var connOptions = {
+        serverNode: '196cb690-e735-4a5c-b3e9-92fd185b82af.hana.trial-us10.hanacloud.ondemand.com:443',
+        encrypt: 'true',
+        sslValidateCertificate: 'false',
+        uid: 'TINY1_HDI_DB_1_2ZXJYQCE5B67FWTPDHHODV4P1_RT',
+        pwd: 'Bg7enaH9rP8z4L1SWnioex4WST-mSz.0wc2HLkCHmaiCFq5Hy7AZKrSJbINZcllX7ClgvmpyZ1SPDXxMW9eA7GEpc4_.VlSN39B6peG9eFoUtu1b9IE.G7nMmwFM_LBk'
+    };
+    
+    var connection = await hana.createConnection();
+
+    connection.connect(connOptions);
+
+    var sql = 'CALL "TINY1_HDI_DB_1"."get_ent1"(RESULT => ?)';
+
+    var rows = await connection.exec(sql);
+
+    return rows;
+}
+
+
 const hdiConnect = () => {
+//API alternativa... não chegou a funcionar pra mim.
     const { HDI } = require('@sap/hdi');
 
     // This credentials object will be directly passed to the @sap/hana-client client. Any options accepted by the @sap/hana-client client can be passed.
